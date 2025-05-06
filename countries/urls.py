437ffa0b1285812b8a80c8eaf15adeb30b15fc8b
Shoumique Hasan <shoumique@gmail.com>
2025-05-06 14:23:31 +0600

    feat(api): Add endpoint to create a new country

--- countries/urls.py
+++ countries/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CountryListView, CountryDetailView
 
 urlpatterns = [
-    path('api/countries/', CountryListView.as_view(), name='country-list'),
+    path('api/countries/', CountryListView.as_view(), name='country-list-create'),
     path('api/countries/<str:cca3>/', CountryDetailView.as_view(), name='country-detail'),
 
 ]
--- countries/views.py
+++ countries/views.py
@@ -2,11 +2,12 @@ from rest_framework import generics
 from .models import Country
 from .serializers import CountrySerializer
 
-class CountryListView(generics.ListAPIView):
+class CountryListView(generics.ListCreateAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
 class CountryDetailView(generics.RetrieveAPIView):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
-    lookup_field = 'cca3'  # or 'cca3' if preferred
+    lookup_field = 'cca3'
+
