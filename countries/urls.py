e3455f94862186020ccc3aee8f530985c630d31c
Shoumique Hasan <shoumique@gmail.com>
2025-05-06 16:30:58 +0600

    feat(auth): add login/logout views and secure country pages with authentication

--- countries/urls.py
+++ countries/urls.py
@@ -8,6 +8,7 @@ from .views import (
     country_list,
     country_detail
 )
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('api/countries/', CountryListCreateView.as_view(), name='country-list-create'),
@@ -18,4 +19,6 @@ urlpatterns = [
 
     path('web/', country_list, name='country_list'),
     path('web/<int:country_id>/', country_detail, name='country_detail'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
